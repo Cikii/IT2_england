afcde6be202ea6e810c026b9bb6e13874e42cfea
Cikii <cikislender@gmail.com>
2021-04-13 21:46:53 +0200

    kompletni uvodni stranka

---
+++ js/js.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#picture').on('click',function(){
+        $(this).attr("src", "img/britanie.jpg");
+        $('#popis').text("Vlajka Británie");    
+    }); 
+  });    
