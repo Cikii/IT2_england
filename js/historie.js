6fa577cfceecc5912a9a710c0366911dc4410134
Cikii <cikislender@gmail.com>
2021-05-05 19:23:20 +0200

    historie

---
+++ js/historie.js
@@ -0,0 +1,234 @@
+const events = [{
+      "year": "1707",
+      "event": "Sjednocení se Skotskem a Irskem",
+      "detail": "Zákon o sjednocení anglického a skotského království z roku 1707 zrušil anglický i skotský parlament, vytvořil království Velké Británie a parlament Velké Británie. Zákon o sjednocení z roku 1800 formálně přičlenil k tomuto království Irsko a od 1. ledna 1801 vytvořil Spojené království Velké Británie a Irska, jehož hlavním městem se stal Londýn.",
+      "url": "https://cs.wikipedia.org/wiki/Z%C3%A1kony_o_unii_z_roku_1707",
+      "evaluation": "true"
+   },
+   {
+      "year": "600",
+      "event": "Křesťanství a vláda sedmi království Slavníkovců",
+      "detail": "Křesťanství se mezi Anglosasy začalo šířit okolo roku 600. Prvním arcibiskupem canterburským byl ustanoven roku 597 Augustin z Canterbury. Roku 601 Augustin pokřtil anglosaského krále Æthelberhta. Křesťanství se pak dále rozšiřovalo v 8. století misiemi Anglosasů i na kontinent a okolo roku 800 se stalo převažujícím náboženstvím i ve Franské říši.",
+      "url": "https://cs.wikipedia.org/wiki/D%C4%9Bjiny_Anglie#K%C5%99es%C5%A5anstv%C3%AD_a_vl%C3%A1da_sedmi_kr%C3%A1lovstv%C3%AD",
+      "evaluation": "false"
+   },
+   {
+      "year": "937",
+      "event": "Bitva u Brunanburhu",
+      "detail": "Bitva u Brunanburhu byla vybojovaná roku 937 mezi silami anglického krále Ethelstana a spojenými silami Olafa Godfridsona, krále Dublinu, Konstantina II., krále Alby, a Owena, krále Strathclyde. Bitva byla reakcí na anglickou invazi do Skotska, započatou v roce 934 pravděpodobně po porušení mírové smlouvy ze strany Konstantina II.. Olafovy síly se v srpnu přepravily z Irska a spojily se se silami spojenců postupujících na anglické území ze severu. K rozhodujícímu střetnutí došlo v měsíci říjnu a skončilo vítězstvím anglického krále, který tak upevnil jednotu anglického království",
+      "url": "https://cs.wikipedia.org/wiki/Bitva_u_Brunanburhu",
+      "evaluation": "true"
+   },
+   {
+      "year": "410",
+      "event": "Římané",
+      "detail": "Římané vpadli pod vedením Julia Caesara do Británie nejprve neúspěšně roku 55 př. n. l. a poté úspěšně následující rok. Do ovládnutí Británie Římany byl počet obyvatel relativně stabilní a jejich jazykem byla kelština. Druhá invaze Římanů byla zahájena v době vlády císaře Claudia roku 43. Během následujících 40 let Římská říše ovládla Anglii,Wales a větší část nížin Skotska a jejich vláda trvala zhruba do roku 410.",
+      "url": "https://cs.wikipedia.org/wiki/D%C4%9Bjiny_Anglie#%C5%98%C3%ADman%C3%A9",
+      "evaluation": "false"
+   },
+   {
+      "year": "899",
+      "event": "Sjednocení Anglie",
+      "detail": "Po Alfrédově smrti roku 899 nastoupil na trůn jeho syn Eduard I. Starší. Ten spolu s Ethelredem zahájili expanzi a pokračoval ve stavbě pevností a opevněných měst po vzoru Alfréda. Jeho syn Ethelstan pokračoval v rozšiřování území svého vlivu a byl prvním králem, který ovládal celé území, jež zahrnuje dnešní Anglii.",
+      "url": "https://cs.wikipedia.org/wiki/D%C4%9Bjiny_Anglie#Sjednocen%C3%AD_Anglie",
+      "evaluation": "true"
+   },
+   {
+      "year": "1453",
+      "event": "Války růží",
+      "detail": "Když anglické vojsko roku 1453 zažilo ve stoleté válce definitivní porážku, král se zhroutil a Richard Plantagenet byl jmenován protektorem Anglie. Jeho opatření vedoucí k hospodářské obnově a posílení bezpečnosti však král poté, co nabyl roku 1455 vědomí, zrušil. York musel Londýn opustit ještě stejného roku došlo k první bitvě mezi Lancastery a Yorky. Vítězství v této bitvě a další záchvat krále znamenaly pro Yorka návrat do funkce protektora, setkal se však se silným odporem královny Markéty z Anjou. ",
+      "url": "https://cs.wikipedia.org/wiki/D%C4%9Bjiny_Anglie#V%C3%A1lky_r%C5%AF%C5%BE%C3%AD",
+      "evaluation": "false"
+   },
+];
+
+const heroes = [{
+      "name": "Alžběta I.",
+      "birth": "7.9.1533",
+      "death": "24.3.1603",
+      "biography": "Alžběta I. (7. září 1533 – 24. březen 1603) byla královna Anglie a Irska od 17. listopadu 1558 až do své smrti. Byla dcerou Jindřicha VIII. a jeho druhé ženy Anny Boleynové a stala se pátou a poslední panovnicí z rodu Tudorovců. Na trůn nastoupila po smrti své nevlastní sestry Marie I., za jejíž vlády byla pro podezření z podpory protestantského povstání téměř rok vězněna. Dokázala sestavit kvalitní královskou radu a spoléhala se na rady svých poradců vedených Williamem Cecilem, 1. baronem z Burghley. Jedním z prvních počinů její vlády bylo upevnění anglikánské (protestantské) církve. I když byla opatrná v zahraničních výbojích, porážka španělské Armady roku 1588 jí přinesla nehynoucí slávu a tato událost bývá označována jako jedno z největších vítězství v anglické historii. Asi dvacet let po své smrti začala být oslavována jako panovnice zlatého věku.",
+      "portraits": [
+         "alzbeta1.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Al%C5%BEb%C4%9Bta_I."
+   },
+   {
+      "name": "Alžběta II.",
+      "birth": "2.6.1926",
+      "death": "x",
+      "biography": "Alžběta II. (anglicky Elizabeth II, celým jménem Elizabeth Alexandra Mary; * 21. dubna 1926 Mayfair, Londýn) je v současné době vládnoucí královna šestnácti nezávislých států označovaných jako Commonwealth realm. Jedná se o státy Antigua a Barbuda, Austrálie, Bahamy, Barbados, Belize, Grenada, Jamajka, Kanada, Nový Zéland, Papua Nová Guinea, Svatý Kryštof a Nevis, Svatá Lucie, Svatý Vincenc a Grenadiny, Šalomounovy ostrovy, Tuvalu a Spojené království Velké Británie a Severního Irska. V každé této zemi je považována za panovnici samostatného státu a plnění královských povinností pro každou zemi je oddělené",
+      "portraits": [
+         "alzbeta2.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Al%C5%BEb%C4%9Bta_II."
+   },
+   {
+      "name": "Winston Churchill",
+      "birth": "30.10.1874",
+      "death": "24.1.1965",
+      "biography": "Sir Winston Leonard Spencer-Churchill [ˈwɪnstən ˈtʃɜrtʃɪl] (30. listopadu 1874 zámek Blenheim, Woodstock, Oxfordshire – 24. ledna 1965 Londýn) byl britský politik, státník, premiér Spojeného království v letech 1940–1945 a 1951–1955. Působil také jako žurnalista, spisovatel (Nobelova cena za literaturu 1953), historik, voják, malíř a zákonodárce.",
+      "portraits": [
+         "churchill.jpg",
+         "churchill1.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Winston_Churchill"
+   },
+   {
+      "name": "Charles Dickens",
+      "birth": "7.2.1812",
+      "death": "9.6.1870",
+      "biography": "Charles Dickens (7. února 1812, Portsmouth – 9. června 1870, Gadshill) byl britský spisovatel. Je považován za jednoho z největších romanopisců 19. století. V dětství žil v bídě, jeho otec byl úředník, který se po přestěhování rodiny do Londýna dokonce ocitl ve vězení pro dlužníky. Dickens si musel sám vydělávat na živobytí. Jako desetiletý chlapec pracoval v továrně na leštidla na boty. Později, když zvládl těsnopis, pracoval jako písař. Od svých dvaceti let působil v listu Morning Chronicle.",
+      "portraits": [
+         "dickens.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Charles_Dickens"
+   },
+   {
+      "name": "Stephen Hawking",
+      "birth": "8.1.1942",
+      "death": "14.3.2018",
+      "biography": "Stephen William Hawking (8. ledna 1942 Oxford – 14. března 2018 Cambridge) byl anglický teoretický fyzik, kosmolog a spisovatel. Na Univerzitě v Cambridgi působil jako ředitel výzkumu Centra teoretické kosmologie a mezi lety 1979–2009 držel pozici lukasiánského profesora matematiky.[1][2] Jeho vědecká činnost zahrnovala spolupráci s Rogerem Penrosem na teorémech gravitačních singularit v rámci obecné relativity a teoretické předpovědi tepelného záření emitovaného černými dírami, známého pod názvem Hawkingovo záření.",
+      "portraits": [
+         "hawking.jpg"
+
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Stephen_Hawking"
+   },
+   {
+      "name": "William Shakespeare",
+      "birth": "26.4.1564",
+      "death": "23.4.1616",
+      "biography": "William Shakespeare (pokřtěn 26. dubna 1564 – zemřel 3. května 1616 podle gregoriánského kalendáře) byl anglický básník, dramatik a herec, který bývá považován za největšího anglicky píšícího spisovatele a celosvětově nejvýznamnějšího dramatika. Je často nazýván anglickým národním básníkem a „bardem z Avonu“.",
+      "portraits": [
+         "shakespeare.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/William_Shakespeare"
+   },
+   {
+      "name": "Paul McCartney",
+      "birth": "18.6.1942",
+      "death": "x",
+      "biography": "Sir James Paul McCartney CH MBE (* 18. června 1942 Liverpool) je anglický hudebník, zpěvák, skladatel a producent, který se proslavil jako zpěvák a baskytarista skupiny The Beatles. Skladatelská dvojice Lennon/McCartney zůstává nejúspěšnější v historii.[1] Poté, co se skupina v roce 1970 rozpadla, se vydal na sólovou dráhu a se svou první ženou Lindou a Dennym Lainem založil kapelu Wings.",
+      "portraits": [
+         "mccartney.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Paul_McCartney"
+   },
+   {
+      "name": "Isaac Newton",
+      "birth": "25.15.1642",
+      "death": "31.3.1727",
+      "biography": "Isaac Newton ([ˌaɪzək ˈnjuːtən]IPA 25. prosince 1642jul./ 4. ledna 1643greg. – 20. březnajul./ 31. března 1727greg. v Londýně) byl anglický fyzik, matematik (působící v Cambridge na stolici Lukasiánského profesora), astronom, alchymista a teolog, jenž bývá často považován za jednu z nejvlivnějších osob v dějinách lidstva.[2] Jeho publikace Philosophiæ Naturalis Principia Mathematica, vydaná v roce 1687, položila základy klasické mechaniky a dnes bývá řazena mezi nejdůležitější knihy v historii vědy.",
+      "portraits": [
+         "newton.jpg"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Isaac_Newton"
+   },
+   {
+      "name": "Agatha Christie",
+      "birth": "15.9.1890",
+      "death": "12.1.1976",
+      "biography": "Agatha Mary Clarissa lady Mallowanová, rozená Millerová, obvykle známá jako Agatha Christie (v češtině někdy i přechýleně Christieová, 15. září 1890 Torquay, Spojené království – 12. ledna 1976 Wallingford, Spojené království), občas používající pseudonym Mary Westmacott byla anglická prozaička, autorka velmi oblíbených kriminálních a detektivních příběhů.",
+      "portraits": [
+         "christie.png"
+      ],
+      "online": "https://cs.wikipedia.org/wiki/Agatha_Christie"
+   },
+
+];
+
+$(function () {
+   $("h2").on("click", function () {
+      $(this).parents(".row").next().toggle(1000);
+   });
+
+   events.forEach((event) => {
+      $("#udalosti tbody").append(`<tr>
+            <td class="event-year">${event.year}</td>
+            <td>
+              <p class="event-name"> <a class="odkaz" href="${event.url}" target="_new">${event.event}</a></p>
+              <p class="event-detail">${event.detail}</p>
+            </td>            
+        </tr>`);
+   });
+
+   $(".event-detail").hide();
+   $(".event-name i, .event-name a").on("mouseover", function () {
+      $("#udalosti tr").removeClass("bg-secondary text-white");
+      $(this).parents("tr").addClass("bg-secondary text-white");
+      $(".event-detail").hide();
+      $(this).parent().next().show(500);
+   });
+
+
+   heroes.forEach((hero) => {
+      $("#postavy .list-group").append(`<li class="list-group-item list-group-item-action">${hero.name}</li>`);
+   });
+
+
+   function fillPersonCard(person) {
+      let hero = heroes.find(item => {
+         return item.name === person
+      });
+
+      $(".card-header").html(`<i class="fas fa-star-of-life"></i> <b>${hero.birth}</b> - <i class="fas fa-cross"></i> <b>${hero.death}</b>`);
+      $(".card-title").text(hero.name);
+      $(".card-text").text(hero.biography);
+      $(".card-footer").html(`Odkaz: <a class="odkaz" href="${hero.online}">${hero.online}</a>`);
+      $(".gallery").empty();
+
+      for (let i = 0; i < hero.portraits.length; i++) {
+         $(".gallery").append(`<div class="col-sm-4"><a href="#"><img src="../img/${hero.portraits[i]}" alt="" class="img-fluid"></a></div>`);
+      }
+   }
+
+
+   $("#postavy li:first").addClass('active');
+   fillPersonCard(heroes[0].name);
+
+
+   $("#postavy li").on("click", function () {
+      $("#postavy li").removeClass("active");
+      $(this).addClass("active");
+
+      let person = $(this).text();
+
+      $("#portret").slideUp(500, function () {
+
+         fillPersonCard(person);
+      });
+
+      $("#portret").slideDown(500);
+   });
+
+
+   $(".article-text").hide();
+
+   $("#zpravodaj h3").on("click", function () {
+      $(this).next(".article-text").toggle();
+   });
+
+
+   $(".likes").on("click", function () {
+      let likes = parseInt($(this).find("span").text());
+      $(this).find("span").text(likes + 1);
+   });
+
+   $(".dislikes").on("click", function () {
+      let dislikes = parseInt($(this).find("span").text());
+      $(this).find("span").text(dislikes + 1);
+   });
+
+
+   let timer = 0;
+   window.setInterval(() => {
+      timer++;
+      $("article figure img").each(function (index, value) {
+         $(value)
+            .fadeOut(500, function () {
+               $(value).attr("src", "images/" + articles[index].gallery[timer % articles[index].gallery.length])
+            })
+            .fadeIn(500);
+      });
+   }, 5000);
+})
